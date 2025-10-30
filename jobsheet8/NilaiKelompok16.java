dcbee2ff064dda9df1037c29253a340acb1d102a
luckyakunbiru@gmail.com <luckyakunbiru@gmail.com>
2025-10-30 11:02:08 +0700

    modfikasi percobaan4

--- jobsheet8/NilaiKelompok16.java
+++ jobsheet8/NilaiKelompok16.java
@@ -3,6 +3,8 @@ public class NilaiKelompok16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int i = 1;
+        double maxRata = 0;
+        int kelompokTerbaik = 0;
         while (i <= 6) {
             System.out.println("Kelompok " + i);
             int totalNilai = 0;
@@ -10,13 +12,19 @@ public class NilaiKelompok16 {
                 System.out.print("Nilai dari Kelompok Penilai " + j + ": ");
                 int nilai = sc.nextInt();
                 totalNilai += nilai;
-            }
+            }    
             double rataNilai = totalNilai / 5.0;
             System.out.println("Kelompok " + i + ": nilai rata-rata = " + rataNilai);
             System.out.println();
+            
+            if (rataNilai > maxRata) {
+                maxRata = rataNilai;
+                kelompokTerbaik = i;
+            }
             i++;
         }
-        
+        System.out.println("Kelompok dengan nilai rata-rata tertinggi: Kelompok " + 
+                          kelompokTerbaik + " dengan nilai " + maxRata);
         sc.close();
     }
 }
