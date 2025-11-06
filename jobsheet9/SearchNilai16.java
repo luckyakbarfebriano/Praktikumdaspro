e3072afafd9a9499b9556fcb4105aed50081a483
luckyakunbiru@gmail.com <luckyakunbiru@gmail.com>
2025-11-06 12:29:16 +0700

    modif no 3 percobaan 4

--- jobsheet9/SearchNilai16.java
+++ jobsheet9/SearchNilai16.java
@@ -3,32 +3,29 @@ import java.util.Scanner;
 public class SearchNilai16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
         int jmlNilai = sc.nextInt();
-        
         int[] arrNilai = new int[jmlNilai];
-        
         for (int i = 0; i < jmlNilai; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             arrNilai[i] = sc.nextInt();
         }
-        
         System.out.print("Masukkan nilai yang ingin dicari: ");
         int key = sc.nextInt();
-        int hasil = 0;
-
+        int hasil = -1;
         for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
                 hasil = i;
                 break;
             }
         }
-        
         System.out.println();
-        System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        if (hasil != -1) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + (hasil + 1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
         System.out.println();
-        
         sc.close();
     }
 }
