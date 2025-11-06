763ab878fe3f3e4a2bd35dc04deeb8017b2d96ba
luckyakunbiru@gmail.com <luckyakunbiru@gmail.com>
2025-11-06 11:58:28 +0700

    modifikasi no 2 percobaan 3

--- jobsheet9/ArrayRataNilai16.java
+++ jobsheet9/ArrayRataNilai16.java
@@ -3,30 +3,36 @@ import java.util.Scanner;
 public class ArrayRataNilai16 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+        System.out.print("Masukkan jumlah mahasiswa : ");
+        int jmlMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jmlMhs];
+        double totalLulus = 0;
+        double totalTidakLulus = 0;
         int hitungLulus = 0;
-        
+        int hitungTidakLulus = 0;
         for (int i = 0; i < nilaiMhs.length; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + " : ");
             nilaiMhs[i] = sc.nextInt();
-        }
-
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-            
             if (nilaiMhs[i] > 70) {
+                totalLulus += nilaiMhs[i];
                 hitungLulus++;
+            } else {
+                totalTidakLulus += nilaiMhs[i];
+                hitungTidakLulus++;
             }
         }
-        
-        rata2 = total / nilaiMhs.length;
-        System.out.println("Rata-rata nilai = " + rata2);
-        System.out.println("Banyaknya mahasiswa yang lulus: " + hitungLulus);
+        if (hitungLulus > 0) {
+            double rataLulus = totalLulus / hitungLulus;
+            System.out.println("Rata-rata nilai lulus = " + rataLulus);
+        } else {
+            System.out.println("Tidak ada mahasiswa yang lulus");
+        }
+        if (hitungTidakLulus > 0) {
+            double rataTidakLulus = totalTidakLulus / hitungTidakLulus;
+            System.out.println("Rata-rata nilai tidak lulus = " + rataTidakLulus);
+        } else {
+            System.out.println("Semua mahasiswa lulus");
+        }
         sc.close();
     }
 }
-
-    
