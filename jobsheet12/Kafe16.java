03644800b6ec084f4733ea45b91c8fa1ae67d6f0
luckyakbarfebriano <luckyakunbiru@gmai.com>
2025-11-27 10:32:49 +0700

    modifikasi percobaan 2

--- jobsheet12/Kafe16.java
+++ jobsheet12/Kafe16.java
@@ -1,23 +1,30 @@
 package jobsheet12;
 public class Kafe16{
-    
-    public static void Menu(String namaPelanggan, boolean isMember) {
-        System.out.println("Selamat datang, " + namaPelanggan + "!");
-        if (isMember) {
-            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
-        }
-        System.out.println("===== MENU RESTO KAFE ====");
-        System.out.println("1. Kopi Hitam - Rp 15,000");
-        System.out.println("2. Cappuccino - Rp 20,000");
-        System.out.println("3. Latte - Rp 22,000");
-        System.out.println("4. Teh Tarik - Rp 12,000");
-        System.out.println("5. Roti Bakar - Rp 10,000");
-        System.out.println("6. Mie Goreng - Rp 18,000");
-        System.out.println("========");
-        System.out.println("Silakan pilih menu yang Anda inginkan.");
+    public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
+    System.out.println("Selamat datang, " + namaPelanggan + "!");
+    if (isMember) {
+        System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
     }
-
+    
+    if ("DISKON50".equals(kodePromo)) {
+        System.out.println("Selamat! Anda mendapatkan diskon 50%!");
+    } else if ("DISKON30".equals(kodePromo)) {
+        System.out.println("Selamat! Anda mendapatkan diskon 30%!");
+    } else {
+        System.out.println("Kode promo invalid!");
+    }
+    
+    System.out.println("===== MENU RESTO KAFE ====");
+    System.out.println("1. Kopi Hitam - Rp 15,000");
+    System.out.println("2. Cappuccino - Rp 20,000");
+    System.out.println("3. Latte - Rp 22,000");
+    System.out.println("4. Teh Tarik - Rp 12,000");
+    System.out.println("5. Roti Bakar - Rp 10,000");
+    System.out.println("6. Mie Goreng - Rp 18,000");
+    System.out.println("========");
+    System.out.println("Silakan pilih menu yang Anda inginkan.");
+}
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Budi", true, "DISKON30");
     }
 }
